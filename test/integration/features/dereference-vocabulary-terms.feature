f8d7835185b33ccb8c7d8f661f8c53a36851ff7a
Brian McBride <brian@epimorphics.com>
2015-08-06 18:55:16 +0100

    Added a test for vocabulary dereferencing

--- test/integration/features/dereference-vocabulary-terms.feature
+++ test/integration/features/dereference-vocabulary-terms.feature
@@ -10,19 +10,12 @@ Feature: list of company profiles
 		Given a visitor
 		When she retrieves the page "/company-profile/def/company-terms/RegisteredCompany"
 		Then she should retrieve a web page
-		And  it should have the title ">LDA resource at /company-profile/def/company-terms/RegisteredCompany.htm"
+		And  it should have the title "LDA resource at /company-profile/def/company-terms/RegisteredCompany.html"
         And  it should have the Companies House Linked Data Viewer banner
-        And it should have outer resources
-        And each outer resource should have a property named 'country of origin'   
-        And each outer resource should have a property named 'identifier'   
-        And each outer resource should have a property named 'incorporation date'   
-        And each outer resource should have a property named 'legal name'   
-        And each outer resource should have a property named 'notation'   
-        And each outer resource should have a property named 'pref label'   
-        And each outer resource should have a property named 'accounts schedule'
-        And each outer resource should have a property named 'org status'   
-        And each outer resource should have a property named 'org type'   
-        And each outer resource should have a property named 'registered address'   
-        And each outer resource should have a property named 'registration'   
-        And each outer resource should have a property named 'returns schedule'       
+        And it should have one outer resource
+        And that outer resource should have a label 'Registered company'
+        And that outer resource should have a type named 'Class'  
+        And that outer resource should have a property named 'label' with literal value 'Registered company'
+        And that outer resource should have a property named 'pref label' with literal value 'Registered company'
+        And that outer resource should have a property named "sub class of' with a resource value named 'Registered Organization'
 	
--- test/integration/features/support/elda-velocity.rb
+++ test/integration/features/support/elda-velocity.rb
@@ -1,10 +1,14 @@
 
 Then(/^it should have outer resources$/) do
-#  expect(page).to have_xpath("//body/div[@class='container']/container/section/[@class='rdf resource outer']")
-#  outer_resource = find(:xpath, "//body/div[@class='container']/section[@class='rdf resource outer']")
   outer_resources = all("section.resource.outer")
   expect(outer_resources.size).not_to be(0)
   @world[:outer_resources] = outer_resources
+end
+
+Then(/^it should have one outer resource$/) do
+  outer_resources = all("section.resource.outer")
+  expect(outer_resources.size).to be(1)
+  @world[:outer_resource] = outer_resources[0]
 end
 
 Then(/^each outer resource should have a property named '(.*)'$/) do | prop_name |
@@ -13,5 +17,31 @@ Then(/^each outer resource should have a property named '(.*)'$/) do | prop_name
     expect(prop_elements.size).not_to be(0)
   end
 end 
+
+Then(/^that outer resource should have a type named '(.*)'$/) do | class_name |
+  resource = @world[:outer_resource]
+  resource_labels = resource.all(:xpath, "div/div[@class='resource-types']//text()[normalize-space(.)='#{class_name}']")
+end
+
+Then(/^that outer resource should have a label '(.*)'$/) do | label |
+  resource = @world[:outer_resource]
+  label_elements = resource.all(:xpath, "div/div/h1[@class='resource-label']")
+  expect(label_elements.size).to eq(1)
+  expect(label_elements[0].text().include?('Registered company')).to be(true)
+  
+end
+
+Then(/^that outer resource should have a property named '(.*)' with literal value '(.*)'$/) do | property_name, value |
+  resource = @world[:outer_resource]
+  prop_elements = resource.all(:xpath, "div/div/ul/div/div/li/div/div[@class='rdf predicate']/a[normalize-space(.)='#{property_name}']")
+  expect(prop_elements.size).not_to be(0) 
+  value_elements = resource.all(:xpath, "div/div/ul/div/div/li/div/div[@class='rdf object literal'][normalize-space(.)='#{value}']")
+  expect(value_elements.size).not_to be(0)
+end
+
+Then(/^that outer resource should have a property named "sub class of' with a resource value named 'Registered Organization'$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
   
 
