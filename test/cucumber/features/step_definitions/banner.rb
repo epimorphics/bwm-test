b29927ada0ba51f05123697309c2aa1b3ef902cf
Brian McBride <brian@epimorphics.com>
2015-08-25 20:48:18 +0100

    update tests

--- test/cucumber/features/step_definitions/banner.rb
+++ test/cucumber/features/step_definitions/banner.rb
@@ -2,6 +2,6 @@
 Then(/^it should have the Companies House Linked Data Viewer banner$/) do
   expect(page).to have_xpath("/html/body/nav/div/div[@class='navbar-header']")
   expect(page).to have_xpath("/html/body/nav/div/div[@class='navbar-header']/div[@class='header-logo']/a[@href='http://www.companieshouse.gov.uk']/span[contains(text(), 'Companies House')]")
-  expect(page).to have_xpath("/html/body/nav/div/div[@class='navbar-header']/div[@class='header-proposition']/*[contains(text(), 'Linked Data Viewer')]")
+  expect(page).to have_xpath("/html/body/nav/div/div[@class='navbar-header']/div[@class='header-proposition']/*[contains(text(), 'Linked Data Service')]")
 end
 
